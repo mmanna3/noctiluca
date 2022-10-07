1e7ff6bbcf3ba9d1a2565dacd90e377995bbac47
mmanna3 <mmmannna3@gmail.com>
2022-10-07 00:47:39 -0300

    Instala Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import {useEffect, useState} from 'react';
 import './App.css';
 import {crearEscrito, escucharEscritos} from './firebase';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import NoteAdd from '@mui/icons-material/NoteAdd';
 
 function App() {
   const [escritos, setEscritos] = useState<any[]>([]);
@@ -16,8 +19,12 @@ function App() {
 
   return (
     <div className="App">
+      <IconButton color="primary" aria-label="upload picture" component="label">
+        <input hidden accept="image/*" type="file" />
+        <NoteAdd />
+      </IconButton>
       {Object.keys(escritos).map((escrito: any) => <div key={escrito}>{escritos[escrito].titulo}</div>)}
-      <button onClick={() => crearEscrito('Che che che', 'bla sa dlba cuerpito eas das as ')}>Crear escrito</button>
+      {/* <button onClick={() => crearEscrito('Che che che', 'bla sa dlba cuerpito eas das as ')}>Crear escrito</button> */}
     </div>
   );
 }
