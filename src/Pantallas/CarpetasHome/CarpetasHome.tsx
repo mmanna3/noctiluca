cbbb405e07095f3e989a74dfbeac19ffe2cb9616
mmanna3 <mmmannna3@gmail.com>
2023-06-26 00:56:13 -0300

    Importa íconos como corresponde

--- src/Pantallas/CarpetasHome/CarpetasHome.tsx
+++ src/Pantallas/CarpetasHome/CarpetasHome.tsx
@@ -2,6 +2,7 @@ import { auth } from "../../firebase";
 import ListaDeCarpetas from "./ListaDeCarpetas";
 import usarNavegacion from "../../usarNavegacion";
 import Boton from "../../components/boton";
+import { PlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const CarpetasHome = () => {
 
@@ -11,17 +12,13 @@ const CarpetasHome = () => {
 		<div className="p-3">
 			<div className="flex justify-between w-full">
 				<Boton soloBorde>/</Boton>
-				<Boton soloBorde onClick={irANuevaCarpeta} className="rounded-full h-12 w-12 px-3">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-						<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-					</svg>
+				<Boton onClick={irANuevaCarpeta} className="rounded-full h-12 w-12 px-3">
+					<PlusIcon />
 				</Boton>
 			</div>
 			<ListaDeCarpetas />
-			<Boton soloBorde className="w-40 mt-2 flex justify-between" onClick={() => {auth.signOut(); localStorage.removeItem("noctiluca.uid");}}>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-				</svg>				
+			<Boton soloBorde className="mt-2 w-44 h-11 flex justify-around items-center" onClick={() => {auth.signOut(); localStorage.removeItem("noctiluca.uid");}}>
+				<XMarkIcon className="w-6"/>		
 				Cerrar sesión
 			</Boton>
 		</div>
