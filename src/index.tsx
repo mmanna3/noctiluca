32f839d446235e13ffa789d690fd8347dee31947
mmanna3 <mmmannna3@gmail.com>
2023-06-26 01:21:47 -0300

    Elimina el CssBaseline de MUI

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
-import CssBaseline from "@mui/material/CssBaseline";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -13,7 +12,6 @@ import "@fontsource/roboto/700.css";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<>
-		<CssBaseline />
 		<App />
 	</>,
 );
