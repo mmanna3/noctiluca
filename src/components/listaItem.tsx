6ab3ca19d236b3c0e2551bde6fe69091fc39a59d
mmanna3 <mmmannna3@gmail.com>
2023-06-28 15:56:11 -0300

    Refactor lista de escritos

--- src/components/listaItem.tsx
+++ src/components/listaItem.tsx
@@ -9,7 +9,7 @@ const ListaItem = (props: IProps) => {
 
 	return <div className="px-2 py-3 text-sm border-b" onClick={props.onClick}>
 		<p className="text-base">{props.titulo}</p>
-		<p className="text-sm text-gray-600">{props.subtitulo}</p>
+		<p className="text-sm text-gray-500">{props.subtitulo}</p>
 	</div>
 	;
 };
