a5f75972a38c975338b9308e5907f895b306f1dd
mmanna3 <mmmannna3@gmail.com>
2023-07-19 13:38:18 -0300

    Tiene acceso hasta que se demuestre lo contrario

--- src/Pantallas/UseSeguridad.tsx
+++ src/Pantallas/UseSeguridad.tsx
@@ -1,33 +1,24 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../AppContext";
-import { configuracion } from "../firebase";
 import usarNavegacion from "../usarNavegacion";
 import { fechaEsDeHaceMenosDe5Minutos } from "../utilidades";
 
 const CARPETAS_QUE_REQUIEREN_ACCESO = ["Ξ", "diario"]; 
 
 const UseSeguridad = ()  => {
-	const [tieneAcceso, setTieneAcceso] = useState<boolean>();
+	const [tieneAcceso, setTieneAcceso] = useState<boolean>(true);
 	const {estado} = useAppContext(); 
 	const {carpetaId} = usarNavegacion();
 
 	useEffect(() => {
-		const obtenerPasswordCorrecto = async () => {
-			const passwordCorrecto = await configuracion.obtenerPassword();
-
-			console.log("el password de la config es: ", passwordCorrecto);
-
+		const obtenerPasswordCorrecto = () => {
 			if ((carpetaId && !CARPETAS_QUE_REQUIEREN_ACCESO.includes(carpetaId)) || (
-				estado.password == passwordCorrecto && 
 				estado.fechaHoraQueIngresoElPassword && 
 				fechaEsDeHaceMenosDe5Minutos(new Date(estado.fechaHoraQueIngresoElPassword)))) {
 				setTieneAcceso(true);
 				console.log("seteando tiene acceso en true");
-			}
-				
-			else 
+			} else 
 				setTieneAcceso(false);
-
 		};
           
 		obtenerPasswordCorrecto();
