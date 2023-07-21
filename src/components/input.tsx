79d5613d5bfc7dafc972e2ed7175ff99b9f1af51
mmanna3 <mmmannna3@gmail.com>
2023-07-20 21:44:20 -0300

    Font medium

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -17,7 +17,7 @@ const Input = (props: Props) => {
 			value={props.valor} 
 			placeholder={props.placeholder} 
 			onChange={props.cuandoCambie} 
-			className={`border-b text-slate-900 w-full text-lg px-2 py-2  focus:outline-none ${props.sinBorde && "border-none"} ${props.textoReGrande && "text-2xl"}`}/>
+			className={`border-b text-slate-900 w-full text-lg px-2 py-2 font-medium focus:outline-none ${props.sinBorde && "border-none"} ${props.textoReGrande && "text-2xl"}`}/>
 		{props.hayError ? 
 			<p className="mt-2 px-2 text-red-600">{props.mensajeError}</p> : ""}
 	</>;
