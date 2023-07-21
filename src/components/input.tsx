dd1229896d8e59e753f52c3fd0bcc49617863ad8
mmanna3 <mmmannna3@gmail.com>
2023-07-20 21:21:44 -0300

    Nueva escrito a Tailwind

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -4,14 +4,22 @@ interface Props {
     placeholder: string;
     hayError: boolean;
     mensajeError: string;
-    autoFocus: boolean;    
+    autoFocus: boolean;
+	sinBorde?: boolean;
+	textoReGrande?: boolean;
 }
 
 const Input = (props: Props) => {
 	
 	return <>
-		<input autoFocus={props.autoFocus} value={props.valor} placeholder={props.placeholder} onChange={props.cuandoCambie} className="border-b w-full text-lg px-2 py-2 focus:outline-none"/>
-		{props.hayError ? <p className="mt-2 px-2 text-red-600">{props.mensajeError}</p> : ""}
+		<input 
+			autoFocus={props.autoFocus} 
+			value={props.valor} 
+			placeholder={props.placeholder} 
+			onChange={props.cuandoCambie} 
+			className={`border-b text-slate-900 w-full text-lg px-2 py-2  focus:outline-none ${props.sinBorde && "border-none"} ${props.textoReGrande && "text-2xl"}`}/>
+		{props.hayError ? 
+			<p className="mt-2 px-2 text-red-600">{props.mensajeError}</p> : ""}
 	</>;
 };
 
