083ac5af91a1c80743c850b3fe59f0bf4d199822
mmanna3 <mmmannna3@gmail.com>
2023-07-22 18:50:43 -0300

    Agrega padding top mas piola

--- src/Pantallas/Matriz.tsx
+++ src/Pantallas/Matriz.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Matriz = () => {
 	return (
 		<div className="min-h-screen">
-			<main className="container mx-auto max-w-4xl md:p-8 py-5 px-5">
+			<main className="container mx-auto max-w-4xl md:px-8 py-8 px-5">
 				<Outlet />
 			</main>
 		</div>
