058f2b9e6d4f4820421298beb1c8f5107890f87c
mmanna3 <mmmannna3@gmail.com>
2025-08-24 01:29:27 -0300

    ver escrito

--- src/Pantallas/VerEscrito.tsx
+++ src/Pantallas/VerEscrito.tsx
@@ -1,78 +1,77 @@
+import { api } from "@/api/api";
+import useApiQuery from "@/api/custom-hooks/use-api-query";
 import { ChevronLeftIcon, TrashIcon } from "@heroicons/react/24/solid";
-import { useEffect, useState } from "react";
-import { IEscrito } from "../Interfaces";
+import { useState } from "react";
 import { Boton } from "../components/botones";
 import Cuerpo from "../components/cuerpo";
 import Encabezado from "../components/encabezado";
 import Input from "../components/input";
 import ChequearSiRequierePassword from "../components/requierePassword";
 import Textarea from "../components/textarea";
-import { editarEscrito, eliminarEscrito, obtenerEscrito } from "../firebase";
+import { editarEscrito, eliminarEscrito } from "../firebase";
 import usarNavegacion from "../usarNavegacion";
 
-
 const VerEscrito = () => {
+	const { volverAEscritosHome, carpetaId, escritoId } = usarNavegacion();
 
-	const {volverAEscritosHome, carpetaId, escritoId} = usarNavegacion();
-	
+	const { data, isLoading, isError } = useApiQuery({
+		key: ["escrito" + escritoId],
+		fn: async () => await api.notaGET(Number(escritoId)),
+	});
+
 	const [titulo, setTitulo] = useState("");
 	const [cuerpo, setCuerpo] = useState("");
 
-	useEffect(() => {
-		const callback = (_escrito: IEscrito) => {
-			console.log(_escrito);
-			setTitulo(_escrito.titulo);
-			setCuerpo(_escrito.cuerpo);
-		};
-
-		if (carpetaId && escritoId)
-			obtenerEscrito(carpetaId, escritoId, callback);
-			
-	}, [escritoId]);
-
 	const editarYVolver = () => {
 		if (carpetaId && escritoId && titulo != "")
-			editarEscrito(carpetaId, {id: escritoId, titulo, cuerpo, fechaHora: ""});
+			editarEscrito(carpetaId, { id: escritoId, titulo, cuerpo, fechaHora: "" });
 		volverAEscritosHome();
 	};
 
 	const eliminar = () => {
-		if (carpetaId && escritoId)
-			eliminarEscrito(carpetaId, escritoId);
+		if (carpetaId && escritoId) eliminarEscrito(carpetaId, escritoId);
 		volverAEscritosHome();
 	};
 
-	return <ChequearSiRequierePassword>
-		<Encabezado>
-			<Boton soloBorde className="flex justify-between items-center" onClick={editarYVolver}>
-				<ChevronLeftIcon className="w-4 h-4 mr-2" />{carpetaId}/{escritoId}
-			</Boton>
-			<Boton soloBorde className="border-none text-slate-400" onClick={eliminar}>
-				<TrashIcon className="h-5 w-5" />
-			</Boton>
-		</Encabezado><Cuerpo>
-			<Input 
-				valor={titulo}
-				sinBorde
-				autoFocus 
-				placeholder="Título"
-				textoReGrande
-				cuandoCambie={(e: React.ChangeEvent<HTMLInputElement>) => setTitulo(e.target.value)} 
-				// hayError={errorTitulo.length > 0} 
-				// mensajeError={errorTitulo}
-			/>
-			<div className="pt-2">
-				<Textarea 
-					valor={cuerpo}
+	if (isLoading) return <div>Cargando...</div>;
+	if (isError) return <div>Error al cargar el escrito</div>;
+	if (!data) return <div>No se encontró el escrito</div>;
+
+	return (
+		<ChequearSiRequierePassword>
+			<Encabezado>
+				<Boton soloBorde className='flex justify-between items-center' onClick={editarYVolver}>
+					<ChevronLeftIcon className='w-4 h-4 mr-2' />
+					{carpetaId}/{escritoId}
+				</Boton>
+				<Boton soloBorde className='border-none text-slate-400' onClick={eliminar}>
+					<TrashIcon className='h-5 w-5' />
+				</Boton>
+			</Encabezado>
+			<Cuerpo>
+				<Input
+					valor={data.titulo}
 					sinBorde
-					placeholder="Texto"
-					cuandoCambie={(e: React.ChangeEvent<HTMLTextAreaElement>) => setCuerpo(e.target.value)} 
-					// hayError={errorTitulo.length > 0} 
+					autoFocus
+					placeholder='Título'
+					textoReGrande
+					cuandoCambie={(e: React.ChangeEvent<HTMLInputElement>) => setTitulo(e.target.value)}
+					// hayError={errorTitulo.length > 0}
 					// mensajeError={errorTitulo}
 				/>
-			</div>
-		</Cuerpo>
-	</ChequearSiRequierePassword>;
+				<div className='pt-2'>
+					<Textarea
+						valor={data.cuerpo || ""}
+						sinBorde
+						placeholder='Texto'
+						cuandoCambie={(e: React.ChangeEvent<HTMLTextAreaElement>) => setCuerpo(e.target.value)}
+						// hayError={errorTitulo.length > 0}
+						// mensajeError={errorTitulo}
+					/>
+				</div>
+			</Cuerpo>
+		</ChequearSiRequierePassword>
+	);
 };
 
 export default VerEscrito;
